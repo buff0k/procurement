460d7f4342da0692757fa35e71c8ea3b736796ee
buff0k <buff0k@gmail.com>
2024-07-29 13:06:15 +0000

    new file:   procurement/procurement/doctype/__init__.py 	new file:   procurement/procurement/doctype/purchase_requisition/__init__.py 	new file:   procurement/procurement/doctype/purchase_requisition/purchase_requisition.js 	new file:   procurement/procurement/doctype/purchase_requisition/purchase_requisition.json 	new file:   procurement/procurement/doctype/purchase_requisition/purchase_requisition.py 	new file:   procurement/procurement/doctype/purchase_requisition/test_purchase_requisition.py 	new file:   procurement/procurement/doctype/purchase_requisition_list/__init__.py 	new file:   procurement/procurement/doctype/purchase_requisition_list/purchase_requisition_list.json 	new file:   procurement/procurement/doctype/purchase_requisition_list/purchase_requisition_list.py 	new file:   procurement/procurement/print_format/__init__.py 	new file:   procurement/procurement/print_format/purchase_requisition/__init__.py 	new file:   procurement/procurement/print_format/purchase_requisition/purchase_requisition.json Add initial DocTypes

---
+++ procurement/procurement/doctype/purchase_requisition/purchase_requisition.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Isambane Mining (Pty) Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Purchase Requisition", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ procurement/procurement/doctype/purchase_requisition_list/purchase_requisition_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Isambane Mining (Pty) Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PurchaseRequisitionList(Document):
+	pass
